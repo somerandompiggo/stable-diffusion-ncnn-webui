7ca20aca928c77c522e06671e66cf13c10339b80
somerandompiggo <somerandompiggo@gmail.com>
2023-03-17 13:57:38 +0000

    Again, gtg.

--- src/main.go
+++ src/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, world!")
+	http.ServeFile(w, r, "../web/index.html")
+}
+
+func getStyle(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../web/css")
 }
 
 func main() {
 	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/style.css", getStyle)
 	fmt.Println("Starting HTTP server on port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
