3f58f6814703fee19ebddf07fa79e252d647c195
somerandompiggo <45299425+somerandompiggo@users.noreply.github.com>
2023-03-18 22:28:07 +0000

    Issue with /images, not sure why

--- src/main.go
+++ src/main.go
@@ -9,13 +9,18 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "../web/index.html")
 }
 
-func getStyle(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "../web/css")
-}
-
 func main() {
 	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/style.css", getStyle)
+	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../web/css/main.css")
+	})
+	http.HandleFunc("/font/inter.ttf", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../web/font/inter.ttf")
+	})
+
+	fs := http.FileServer(http.Dir("../web/images/"))
+	http.Handle("/images", fs)
+
 	fmt.Println("Starting HTTP server on port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
