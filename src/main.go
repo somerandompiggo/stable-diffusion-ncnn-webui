4c2ac07067332b212f705ab3fd8308510b7f14fd
somerandompiggo <45299425+somerandompiggo@users.noreply.github.com>
2023-03-20 07:50:18 +0000

    Almost able to generate images!

--- src/main.go
+++ src/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
 )
 
 func magicFile(width int, height int, speed int, stepcount int, seed int, positive string, negative string) string {
-	return fmt.Sprint(width) + "\n" + fmt.Sprint(height) + "\n" + fmt.Sprint(speed) + "\n" + fmt.Sprint(stepcount) + "\n" + fmt.Sprint(seed) + "\n" + positive + "\n" + negative
+	return fmt.Sprint(height) + "\n" + fmt.Sprint(width) + "\n" + fmt.Sprint(speed) + "\n" + fmt.Sprint(stepcount) + "\n" + fmt.Sprint(seed) + "\n" + positive + "\n" + negative
 }
 
 func check(err error) {
@@ -17,13 +19,34 @@ func check(err error) {
 	}
 }
 
+func generate(w http.ResponseWriter, r *http.Request) {
+	reqparams := r.URL.Query()
+
+	width, err := strconv.Atoi(reqparams.Get("width"))
+	check(err)
+	height, err := strconv.Atoi(reqparams.Get("height"))
+	check(err)
+	steps, err := strconv.Atoi(reqparams.Get("numSteps"))
+	check(err)
+	promptP := reqparams.Get("positivePrompt")
+	promptN := reqparams.Get("negativePrompt")
+
+	SDGenerate(magicFile(width, height, 1, steps, 92587, promptP, promptN))
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func SDGenerate(magicfile string) {
+	err := os.WriteFile("../build/magic.txt", []byte(magicfile), 0644)
+	check(err)
+
+	cmd := exec.Command("../build/stable-diffusion-ncnn")
+}
+
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	if _, err := os.Stat("../stable-diffusion-ncnn"); os.IsNotExist(err) {
+	if _, err := os.Stat("../build/stable-diffusion-ncnn"); os.IsNotExist(err) {
 		http.ServeFile(w, r, "../web/notpresent.html")
 	} else {
 		http.ServeFile(w, r, "../web/index.html")
-		err := os.WriteFile("./magic.txt", []byte(magicFile(256, 256, 1, 15, 92587, "cat", "dog")), 0644)
-		check(err)
 	}
 }
 func main() {
@@ -37,6 +60,8 @@ func main() {
 	http.HandleFunc("/result.png", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "../web/images/demoimg.png")
 	})
+
+	http.HandleFunc("/generate", generate)
 
 	http.HandleFunc("/", getRoot)
 
